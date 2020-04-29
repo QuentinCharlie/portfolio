aefaaf453122124c22bf47216161899da5ba71c0
QuentinCharlie <quentin.charlie@orange.fr>
2020-04-29 16:51:54 +0200

    video page responsive

--- src/components/AppEnglish/Game/GameStyled.js
+++ src/components/AppEnglish/Game/GameStyled.js
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 const GameStyled = styled.div`
+  height: calc(100vh - 100px);
+  overflow-y: auto;
   .player {
     margin: 0 auto;
     box-shadow: 0 0 5px black;
+    max-width: 95%;
   }
 `;
 
